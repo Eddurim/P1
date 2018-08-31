591c605453074a40781e7fe81d63b55bd5963186
Carolitta <daylic.fi@gmail.com>
2018-08-31 16:10:52 -0500

    Agregue for

--- prueba.sh
+++ prueba.sh
@@ -4,6 +4,19 @@ name=listado=$(lsusb | awk '{ print $7 }')
 listaBlanca=/bin/listaBlanca.txt
 listaNegra=/bin/listaNegra.txt
 
+for dispositivos in $listado; do
+	cad="$(grep -x $dispositivos $listaBlanca)"
+	cad2="$(grep -x $dispositivos $listaNegra)"
+	if [ "$dispositivos" == "$cad" ];
+	then 
+		echo "match"
+elif [ "$dispositivos" == "$cad2" ]
+	then
+		echo -e "USB ($nombre) detectada en lista negra, no se va a montar"
+	else
+		
+
+
 
 
 
